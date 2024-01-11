3d98664801bfd6007fa91fb81c9e9e38ee9eae9d
朱华 <2467352332@qq.com>
2024-01-11 14:40:19 +0800

    3

---
+++ unpackage/dist/build/h5/assets/index.d1c090c0.js
@@ -0,0 +1,1 @@
+import{aG as a}from"./index-f7293766.js";const t=(t,o)=>a({url:t,method:"GET",data:o}),o=(t,o)=>a({url:t,method:"POST",data:o});export{t as g,o as p};
--- unpackage/dist/build/h5/assets/index.d67bd290.js
+++
@@ -1,1 +0,0 @@
-import{aG as a}from"./index-5942a641.js";const t=(t,o)=>a({url:t,method:"GET",data:o}),o=(t,o)=>a({url:t,method:"POST",data:o});export{t as g,o as p};
